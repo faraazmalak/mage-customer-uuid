6793f9baf59f510d77bd542620e259aeb717199e
faraazmalak <faraazmalak@gmail.com>
2023-12-31 11:22:33 +0000

    Implementing a stronger UUID validation logic for customer save/update operations

--- Plugin/CreateUuidAttributeValue.php
+++ Plugin/CreateUuidAttributeValue.php
@@ -7,6 +7,8 @@ use Magento\Customer\Api\CustomerRepositoryInterface;
 use Magento\Customer\Api\Data\CustomerInterface;
 use Magento\Customer\Model\ResourceModel\Customer\CollectionFactory;
 use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\Message\ManagerInterface;
+use Magento\Framework\Message\Notice;
 use Quarry\CustomerUuid\Exception\DuplicateUuidException;
 use Quarry\CustomerUuid\Exception\UuidCreateException;
 use Quarry\CustomerUuid\Exception\UuidValidateException;
@@ -15,57 +17,105 @@ use Ramsey\Uuid\Uuid;
 
 /**
  * Intercepts customer save operation,
- * to create and assign a value to the uuid customer attribute, if the attribute is null
+ * to create and assign a value to the uuid customer attribute, if the attribute is null or an invalid UUID
  */
 class CreateUuidAttributeValue
 {
     private $customerCollection;
     private Logger $logger;
+    private ManagerInterface $messageManager;
+    private string $originalUuid = '';
+    private bool $isNewCustomer = true;
 
     /**
      * @param CollectionFactory $customerCollectionFactory
      * @param Logger $logger
+     * @param ManagerInterface $messageManager
      */
-    public function __construct(CollectionFactory $customerCollectionFactory, Logger $logger)
+    public function __construct(CollectionFactory $customerCollectionFactory, Logger $logger, ManagerInterface $messageManager)
     {
         $this->customerCollection = $customerCollectionFactory->create();
         $this->logger = $logger;
+        $this->messageManager = $messageManager;
     }
 
     /**
-     * This method is triggered before save() in Magento\Customer\Api\CustomerRepositoryInterface
+     *  This method is triggered before save() in Magento\Customer\Api\CustomerRepositoryInterface
      *
      * @param CustomerRepositoryInterface $customerRepository
      * @param CustomerInterface $customer
      * @return CustomerInterface[]
-     * @throws LocalizedException
+     * @throws DuplicateUuidException
+     * @throws UuidCreateException
+     * @throws UuidValidateException
      */
     public function beforeSave(CustomerRepositoryInterface $customerRepository, CustomerInterface $customer): array
     {
-        if ($customer->getCustomAttribute('uuid') === null) {
-            try {
-                $uuid = Uuid::uuid4()->toString();
-            } catch (Exception $e) {
-                $message=__('Error generating a UUID. Try submitting the form again.');
-                throw new UuidCreateException($message, $this->logger, $e);
-            }
-
-            try {
-                $this->customerCollection->getSelect()->limit(1);
-                $this->customerCollection->addAttributeToFilter('uuid', $uuid);
-                $isUuidDuplicate = $this->customerCollection->getSize() > 0;
-            } catch (LocalizedException $e) {
-                $errorMessage = __("Unable to validate uniqueness of UUID $uuid. Try resubmitting the form.");
-                throw new UuidValidateException($errorMessage, $this->logger, $e);
-            }
-
-            if ($isUuidDuplicate) {
-                $message = __("UUID $uuid has already been assigned to another customer. Try resubmitting the form.");
-                throw new DuplicateUuidException($message, $this->logger);
-            } else {
+        // $customer->getId() returns null for new customers
+        $this->isNewCustomer = $customer->getId() === null;
+        if($this->isNewCustomer){
+            $uuid = $this->generateUuid();
+            $customer->setCustomAttribute('uuid', $uuid);
+        }else{
+            $this->originalUuid = $customer->getCustomAttribute('uuid')?->getValue() ?? '';
+            $isUuidValid = @(Uuid::isValid($this->originalUuid)) ?? false;
+            if (!$isUuidValid) {
+                $uuid = $this->generateUuid();
                 $customer->setCustomAttribute('uuid', $uuid);
             }
         }
         return [$customer];
+    }
+
+    /**
+     * This method is triggered after save() in Magento\Customer\Api\CustomerRepositoryInterface
+     * It displays a message, in case uuid is changed for an existing customer
+     *
+     * @param CustomerRepositoryInterface $subject
+     * @param CustomerInterface $customer
+     * @return CustomerInterface
+     */
+    public function afterSave(CustomerRepositoryInterface $subject, CustomerInterface $customer): CustomerInterface
+    {
+
+        $newUuid = $customer->getCustomAttribute('uuid')?->getValue();
+        if (!$this->isNewCustomer && $this->originalUuid !== $newUuid) {
+            $noticeMessage = new Notice(__("Customer UUID has changed, since the previous one was invalid."));
+            $this->messageManager->addMessage($noticeMessage);
+        }
+        return $customer;
+    }
+
+    /**
+     * Generates a UUID for customer
+     *
+     * @return string
+     * @throws DuplicateUuidException
+     * @throws UuidCreateException
+     * @throws UuidValidateException
+     */
+    private function generateUuid(): string
+    {
+        try {
+            $uuid = Uuid::uuid4()->toString();
+        } catch (Exception $e) {
+            $message = __('Error generating a UUID. Try submitting the form again.');
+            throw new UuidCreateException($message, $this->logger, $e);
+        }
+
+        try {
+            $this->customerCollection->getSelect()->limit(1);
+            $this->customerCollection->addAttributeToFilter('uuid', $uuid);
+            $isUuidDuplicate = $this->customerCollection->getSize() > 0;
+        } catch (LocalizedException $e) {
+            $errorMessage = __("Unable to validate uniqueness of UUID $uuid. Try resubmitting the form.");
+            throw new UuidValidateException($errorMessage, $this->logger, $e);
+        }
+
+        if ($isUuidDuplicate) {
+            $message = __("UUID $uuid has already been assigned to another customer. Try resubmitting the form.");
+            throw new DuplicateUuidException($message, $this->logger);
+        }
+        return $uuid;
     }
 }
